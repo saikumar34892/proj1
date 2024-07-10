158ee7f267bce5fb68b5741867e6164622cba87b
saikumar34892 <saikumarmatta3821@gmail.com>
2024-07-10 12:24:16 +0530

    dg

---
+++ src/first.java
@@ -0,0 +1,9 @@
+public class first {
+    public static void main(String[] args) {
+        int count=10;
+        System.out.println("count value"+count);
+        String name = "saikumar";
+        System.out.println("name"+name);
+    }
+    
+}
