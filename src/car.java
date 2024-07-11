22449cd6e7ff1d96f1c08b1c8d8289790637246f
saikumar34892 <saikumarmatta3821@gmail.com>
2024-07-11 12:14:16 +0530

    vc

---
+++ src/car.java
@@ -0,0 +1,8 @@
+public class car {
+    public void run() {
+        System.out.println("run");
+    }  
+    public void horn() {
+        System.out.println("horn");
+    }
+}
--- src/first.java
+++ src/first.java
@@ -1,9 +1,13 @@
 public class first {
     public static void main(String[] args) {
-        int count=10;
-        System.out.println("count value"+count);
-        String name = "saikumar";
-        System.out.println(name+" "+count);
+        car car1=new car();
+        car1.run();
+        car1.horn();
+
+        car car2=new car();
+        car2.run();
+        car2.horn();
     }
+
     
 }
